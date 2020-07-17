ea171d2e2341f974314a62054d2a9fcf273788e7
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-07-17 20:17:09 +1000

    setup heroku stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const app = express()
-app.use(express.json())
 const port = process.env.PORT || 5000
+
+app.use(express.json())
 
 app.get('/test', (req, res) => {
   res.send('working')
