5a57394d00b6c6808163d1b0ca6f6d122d6eb0ed
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-07-17 21:25:08 +1000

    add coffee command endpoint

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ app.use(express.json())
 
 app.get('/test', (req, res) => {
   res.send('working')
-});
+})
 
 app.post('/slack/challenge', (req, res) => {
   const { challenge } = req.body;
   res.send(challenge)
+})
+
+app.post('/command/coffee', (req, res) => {
+  res.send('sent coffee command')
 })
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
