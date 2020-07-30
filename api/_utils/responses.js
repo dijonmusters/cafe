c683a5165f38f64d667a072ca28aa51530fc092c
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-07-30 22:24:30 +1000

    fix help texxt

--- api/_utils/responses.js
+++ api/_utils/responses.js
@@ -3,11 +3,7 @@ import { formatDistanceToNow } from 'date-fns'
 const join = `You have entered the cafe`
 const leave = `You have left the cafe`
 
-const help = `Want some help?
-  :coffee: \`/cafe join\`
-  :non-potable_water: \`/cafe leave\`
-  :man_and_woman_holding_hands: \`/cafe list\`
-`
+const help = `:coffee: \`/cafe join\`\n:non-potable_water: \`/cafe leave\`\n:man_and_woman_holding_hands: \`/cafe list\``
 
 const list = (matches) => {
   return matches.map(m => `${m.guest.username} (${formatDistanceToNow(m.date)} ago)`).join('\n')
