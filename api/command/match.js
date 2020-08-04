e70f1335370b0a4b15e71defb21e7d0bcee0025b
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-08-04 21:20:01 +1000

    add gh action for scheduled matching

--- api/command/match.js
+++ api/command/match.js
@@ -2,8 +2,13 @@ const { sendMatchMessages } = require('../_utils/matcher')
 
 module.exports = async (req, res) => {
   try {
-    await sendMatchMessages()
-    res.send('sent all coffee dates')
+    const { secret } = req.body
+    if (secret === process.env.MATCH_SECRET) {
+      await sendMatchMessages()
+      res.send('sent all coffee dates')
+    } else {
+      res.send('failed to send coffee dates')
+    }
   } catch(e) {
     console.log(e)
     res.send('failed to send coffee dates')
