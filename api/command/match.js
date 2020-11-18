80e5cd38f68a3dc2a3e968736f4ee7c0918e4565
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-11-18 13:39:45 +1100

    trigger deploy

--- api/command/match.js
+++ api/command/match.js
@@ -4,9 +4,12 @@ module.exports = async (req, res) => {
   try {
     const { secret } = req.body
     if (secret === process.env.MATCH_SECRET) {
+      console.log('matching')
       await sendMatchMessages()
+      console.log('matched')
       res.send('sent all coffee dates')
     } else {
+      console.log('failed to match')
       res.send('failed to send coffee dates')
     }
   } catch (e) {
